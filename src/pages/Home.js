c1772de5d6f477e2e79ce88cc4c5ccb04653cdf9
Manuel Martinez <m.martinez45@hotmail.com>
2022-01-25 10:43:02 -0500

    homepage styling v1

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,64 @@
+import { Container, Row, Col} from 'react-bootstrap'
+import { Fade, Slide, SlideProps } from 'react-awesome-reveal'
+import { Link } from 'react-router-dom'
+
+
 function Home(){
-    return <h1>Home Page</h1>
+    const muscleGroups = ['arms', 'back', 'chest', 'core', 'legs']
+    
+    return (
+        <Container fluid id='homePage'>
+                <Row>
+                    <Col>
+                        <Fade duration={2500}>
+                            <img src="https://c4.wallpaperflare.com/wallpaper/972/1015/662/muscles-pose-white-and-black-bodybuilder-wallpaper-preview.jpg" alt="" />
+                        </Fade>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <header>
+                            <Slide onVisibilityChange={true} direction="right" duration={750}>
+                            <h1>
+                                "Victory isn’t defined by wins and losses; it’s defined by effort."
+                            </h1>
+
+                            </Slide>
+                            
+                        </header>
+                    </Col>
+                </Row>   
+
+                <Fade onVisibilityChange={true} duration={2000}>
+                    <Row>
+                        <section>
+                            <Col lg={4}>
+                                <div className="guides">
+                                    <h3>Guides</h3>
+                                    <hr></hr>
+                                    {muscleGroups.map((mg, key) => (
+                                    <Link to={`/exercises/${mg}`} key={key}>
+                                        <p className='homeGuideLinks'>{mg}</p>
+                                    </Link>
+                                ))}
+                                </div>
+                            </Col>
+                            <Col lg={4}>
+                                <div className="workouts">
+                                    <h3>Workouts</h3>
+                                </div>
+                            </Col>
+                            <Col lg={4}>
+                                <div className="nutrition">
+                                    <h3>Nutrition</h3>
+                                </div>
+                                    
+                            </Col>
+                        </section>
+                    </Row>
+                </Fade>
+        </Container>
+    )
 }
 
 export default Home
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,14 +1,19 @@
-import { Container, Row } from 'react-bootstrap'
-
+import { Container, Row, Col } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 function Index(props){
     return(
         <Container id="indexPage">
+            <h1>Exercises Guide List</h1>
             <Row>
                 {props.exercise.map((exercise, key) => {
-                    return <div className="exercise" key={key}>
-                    <img  src={exercise.img} alt={exercise.name} />
-                    <h3>{exercise.name}</h3>
-                </div>
+                    return <Col  className='whatthe'lg={4}>
+                        <div className="float-left exercise" key={key}>
+                        <Link to={`/exercises/${exercise.main}/${exercise.name}`}>
+                            <img  src={exercise.img} alt={exercise.name} />
+                            <h3>{exercise.name}</h3>
+                        </Link>
+                        </div>
+                        </Col>
                 })}
             </Row>
         </Container>
