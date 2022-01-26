8e73c4ae47c403ea918419f891cbd2b9092a5034
Manuel Martinez <m.martinez45@hotmail.com>
2022-01-26 13:50:31 -0500

    muscleGroupPage styling v1

--- src/pages/MuscleGroup.js
+++ src/pages/MuscleGroup.js
@@ -1,22 +1,48 @@
 import { Link } from 'react-router-dom'
-import { Container, Row} from 'react-bootstrap'
+import { Container, Row, Col} from 'react-bootstrap'
 function MuscleGroup(props){
     const muscleGroup = props.match.params.muscleGroup
     const exercises = props.exercise.filter(e => e.main.toLowerCase() === muscleGroup)
     return(
-        <Container>
-            <Row>
-                <h1 id='muscleHeading'>{muscleGroup}</h1>
-                {exercises.map((exercise, key) => (
-                    <div className='exerciseBlock' key={key}>
-                        <Link to={`/exercises/${muscleGroup}/${exercise.name}`} >
-                            <img src={exercise.img} alt={exercise.name} />
-                            <h4>{exercise.name}</h4>
-                        </Link>
-                    </div>
-                ))}
-            </Row>
-        </Container>
+        <Container id='muscleGroupPage'>
+                <Row>
+                    <h1 id='muscleHeading'>{muscleGroup}</h1>
+                    {exercises.map((exercise, key) => (
+                        // <div className='exerciseBlock' key={key}>
+                        //     <Link to={`/exercises/${muscleGroup}/${exercise.name}`} >
+                        //         <h3>{exercise.name}</h3>
+                        //     </Link>
+                        // </div>
+                        <Col className='whatthe' lg={{span: 8, offset: 2}}>
+                            <div className="exercise" key={key}>
+                                <Link to={`/exercises/${muscleGroup}/${exercise.name}`}>
+                                    <h3>{exercise.name}</h3>
+                                </Link>
+                            </div>
+                        </Col>
+                    ))}
+                </Row>
+    </Container>
+
+// <Container id="indexPage">
+//             <h1>
+//                 Exercise Guide List
+//             </h1>
+//             <hr></hr>
+//             <Row>
+//                 {props.exercise.map((exercise, key) => {
+//                     return <Col  className='whatthe'lg={{span: 8, offset: 2}}>
+//                         <div className="exercise" key={key}>
+//                         <Link to={`/exercises/${exercise.main}/${exercise.name}`}>
+//                             {/* <img  src={exercise.img} alt={exercise.name} /> */}
+//                             <h3>{exercise.name}</h3>
+//                         </Link>
+//                         </div>
+//                         </Col>
+//                 })}
+//             </Row>
+//         </Container>
+            
     ) 
 }
 
